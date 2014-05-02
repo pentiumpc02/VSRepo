414ab902a00b567c215a53b5fe195162ab332ca0
Tom Chai <chochai@cisco.com>
2014-05-02 11:50:56 -0400

    init code

--- VSRepo_WinForm_Test/Form1.Designer.cs
+++ VSRepo_WinForm_Test/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(42, 52);
+            this.button1.Location = new System.Drawing.Point(54, 55);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(98, 24);
             this.button1.TabIndex = 0;
