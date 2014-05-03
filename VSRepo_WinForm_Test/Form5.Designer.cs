d06f853848814522274668e51b8ca9e03ea9adf1
unknown <chochai@chochai-WS.cisco.com>
2014-05-02 23:57:26 -0400

    ssss

--- VSRepo_WinForm_Test/Form5.Designer.cs
+++ VSRepo_WinForm_Test/Form5.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 255);
+            this.Name = "Form5";
             this.Text = "Form5";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
